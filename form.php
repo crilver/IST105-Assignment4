631d088574a95ccff8594dbc19a16089b28c01a4
Cristobal <cris_99@hotmail.es>
2025-01-30 15:53:13 -0800

    Feature: Form to ask for 3 input numeric values

---
+++ form.php
@@ -0,0 +1,14 @@
+<html>
+    <form action="process.php" method="post">
+        <label>Input for a (numeric):</label>
+        <input type="number" name="a" required>
+        <br>
+        <label>Input for b (numeric):</label>
+        <input type="number" name="b" required>
+        <br>
+        <label>Input for c (numeric):</label>
+        <input type="number" name="c" required>
+        <br>
+        <input type="submit" value="Calculate">
+    </form>
+</html>
